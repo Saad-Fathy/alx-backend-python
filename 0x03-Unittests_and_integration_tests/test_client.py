af4b973336a9c03eac523067204c104ae9d75e59
Saad Fathy <saad.fathy.abdelkareem.com>
2025-09-21 01:04:16 +0300

    5. Mocking a property

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -3,6 +3,7 @@
 
 import unittest
 from unittest.mock import patch, PropertyMock
+from parameterized import parameterized
 from client import GithubOrgClient
 
 
@@ -45,6 +46,18 @@ class TestGithubOrgClient(unittest.TestCase):
 
         mock_public_repos_url.assert_called_once()
         mock_get_json.assert_called_once_with(mock_public_repos_url.return_value)
+
+    @parameterized.expand([
+        ({"license": {"key": "my_license"}}, "my_license", True),
+        ({"license": {"key": "other_license"}}, "my_license", False),
+    ])
+    def test_has_license(self, repo: dict, license_key: str, expected: bool) -> None:
+        """Test that has_license returns expected result for given repository and
+        license key.
+        """
+        result = GithubOrgClient.has_license(repo, license_key)
+        self.assertEqual(result, expected)
+
 
 if __name__ == "__main__":
     unittest.main()
